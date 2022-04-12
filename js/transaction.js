379403812c15161eb97fba0c43078efd892739f5
MdKaioumislam5 <afrankaiyum34@gmail.com>
2022-04-13 04:59:02 +0600

    balance updated

--- js/transaction.js
+++ js/transaction.js
@@ -26,5 +26,31 @@ document.getElementById("calculate").addEventListener("click", function () {
   balanceInput.innerText = incomeAmount - totalExpenseInput.innerText;
   //   console.log(foodAmount);
 
-//   const balanceTotal = incomeAmount - totalExpenseInput.innerText;
+  //   const balanceTotal = incomeAmount - totalExpenseInput.innerText;
+});
+
+document.getElementById("save-button").addEventListener("click", function () {
+  const saveInput = document.getElementById("save-input");
+  const saveAmountText = saveInput.value;
+  const saveAmount = parseFloat(saveAmountText);
+  //   console.log(foodAmount);
+  const incomeInput = document.getElementById("income-input");
+  const incomeAmountText = incomeInput.value;
+  const incomeAmount = parseFloat(incomeAmountText);
+//   console.log(incomeAmount);
+// get saving amount
+  const savingAmountInput = document.getElementById("saving-amount");
+  const savingAmountText = savingAmountInput.innerText;
+  const savingAmount = parseFloat(savingAmountText);
+  savingAmountInput.innerText = incomeAmount / saveAmount;
+
+  const balanceInput = document.getElementById("balance-input");
+  const balanceAmountText = balanceInput.innerText;
+  const balanceAmount = parseFloat(balanceAmountText);
+  balanceInput.innerText = incomeAmount - totalExpenseInput.innerText;
+//   get remaining balance
+  const remainingBalanceInput = document.getElementById("remaining-balance");
+  const remainingBalanceText = remainingBalanceInput.innerText;
+  const remainingBalance = parseFloat(remainingBalanceText);
+  remainingBalanceInput.innerText = balanceInput.innerText - savingAmountInput.innerText;
 });
