dbdd92c78caf874990402c15a3bc23482a3a52e0
MdKaioumislam5 <afrankaiyum34@gmail.com>
2022-04-21 10:21:03 +0600

    update balance & income

--- js/transaction.js
+++ js/transaction.js
@@ -15,6 +15,7 @@ document.getElementById("calculate").addEventListener("click", function () {
   if (incomeAmount <= 0 || isNaN(incomeAmount)) {
     alert("Income must be typeof number and positive value");
   }
+  // expense amount
   const foodAmount = getInputValue("food-input");
   const rentAmount = getInputValue("rent-input");
   const clothesAmount = getInputValue("clothes-input");
@@ -29,97 +30,43 @@ document.getElementById("calculate").addEventListener("click", function () {
     alert("Income can not be less than expense value!!")
   }
   //   console.log(foodAmount);
-
+// balance amount
   const balanceInput = document.getElementById("balance-input");
   const balanceAmountText = balanceInput.innerText;
   const balanceAmount = parseFloat(balanceAmountText);
   console.log(balanceAmount);
   balanceInput.innerText = incomeAmount - totalExpenseInput.innerText;
-  //   console.log(foodAmount);
-
-  //   const balanceTotal = incomeAmount - totalExpenseInput.innerText;
 });
 
 document.getElementById("save-button").addEventListener("click", function () {
   const saveAmount = getInputValue("save-input");
   const incomeAmount = getInputValue("income-input");
-
+  if((saveAmount <= 0 || isNaN(saveAmount)) || (incomeAmount <= 0 || isNaN(incomeAmount))){
+    alert("Amount must be typeof number and positive value");
+  }
+// save amount
   const savingAmountInput = document.getElementById("saving-amount");
   const savingAmountText = savingAmountInput.innerText;
   const savingAmount = parseFloat(savingAmountText);
   savingAmountInput.innerText = (incomeAmount * saveAmount) / 100;
 
+  // total balance
   const balanceInput = document.getElementById("balance-input");
   const balanceAmountText = balanceInput.innerText;
   const balanceAmount = parseFloat(balanceAmountText);
   balanceInput.innerText = incomeAmount - totalExpenseInput.innerText;
+
   //   get remaining balance
   const remainingBalanceInput = document.getElementById("remaining-balance");
   const remainingBalanceText = remainingBalanceInput.innerText;
   const remainingBalance = parseFloat(remainingBalanceText);
-  // console.log(balanceAmountText);
-  // console.log(savingAmountText);
-  // console.log(savingAmountInput);
+
   remainingBalanceInput.innerText =
     parseFloat(balanceAmount) - parseFloat(savingAmountInput.innerText);
+
+    
+  if(balanceInput.innerText < savingAmountInput.innerText){
+    alert("your don't enough balance to save amount")
+  }
 });
 
-// first
-
-// function updateTotalField(totalFieldId, amount) {
-
-//   const totalExpenseInput = document.getElementById(totalFieldId);
-//   const totalExpenseAmountText = totalExpenseInput.innerText;
-//   const totalExpenseAmount = parseFloat(totalExpenseAmountText);
-//   totalExpenseInput.innerText = totalExpenseAmount + amount;
-// }
-// update expense field
-// function updateExpenseField(expenseFieldId, amount) {
-//   const expenseElement = document.getElementById(totalFieldId);
-//   const expenseText = expenseElement.innerText;
-//   const expenseTotal = parseFloat(expenseText);
-
-//   expenseElement.innerText = expenseTotal + amount;
-
-// const totalExpenseInput = document.getElementById("total-expense");
-// const totalExpenseAmountText = totalExpenseInput.innerText;
-// const totalExpenseAmount = parseFloat(totalExpenseAmountText);
-// totalExpenseInput.innerText = foodAmount + rentAmount + clothesAmount;
-
-// second
-// if (depositAmount > 0) {
-//   updateTotalField("deposit-total", depositAmount);
-//   // updateBalance(depositAmount, true);
-// }
-// const foodInput = document.getElementById("food-input");
-// const foodAmountText = foodInput.value;
-// const foodAmount = parseFloat(foodAmountText);
-//   console.log(foodAmount);
-
-// const rentInput = document.getElementById("rent-input");
-// const rentAmountText = rentInput.value;
-// const rentAmount = parseFloat(rentAmountText);
-//   console.log(foodAmount);
-
-// const clothesInput = document.getElementById("clothes-input");
-// const clothesAmountText = clothesInput.value;
-// const clothesAmount = parseFloat(clothesAmountText);
-//   console.log(foodAmount);
-
-// third
-// const incomeInput = document.getElementById("income-input");
-// const incomeAmountText = incomeInput.value;
-// const incomeAmount = parseFloat(incomeAmountText);
-//   console.log(foodAmount);
-
-// fourth
-
-// const saveInput = document.getElementById("save-input");
-// const saveAmountText = saveInput.value;
-// const saveAmount = parseFloat(saveAmountText);
-// //   console.log(foodAmount);
-// const incomeInput = document.getElementById("income-input");
-// const incomeAmountText = incomeInput.value;
-// const incomeAmount = parseFloat(incomeAmountText);
-//   console.log(incomeAmount);
-// get saving amount
